7e348b859d5e344b6915bc8485505476fc8a4c28
Admin <fanqtv2412@gmail.com>
2023-04-15 13:50:47 +0700

    fix insert img + logic send msg

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChatThreeRole.Controllers
 {
@@ -12,9 +13,54 @@ namespace ChatThreeRole.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string email)
         {
-            return View();
+            var account = _context.Account.Find(email);
+            return View(account);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProfile()
+        {
+            string email = Request.Form["txtEmail"];
+            string fullName = Request.Form["txtFullName"];
+            string passWord = Request.Form["txtPassword"];
+            var account = _context.Account.Find(email);
+            
+            if(account == null)
+            {
+                return RedirectToAction("Index", "Manage", new { email = email });
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(passWord))
+                {
+                    passWord = account.Password;
+                }
+                try
+                {
+                    account.FullName = fullName;
+                    account.Password = passWord;
+                    var file = Request.Form.Files[0];
+                    if (file != null && file.Length > 0)
+                    {
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", file.FileName);
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            file.CopyTo(fileStream);
+                            account.Avatar = file.FileName;
+                        }
+                    }
+                    _context.Account.Update(account);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index", "Manage", new { email = email });
+
+                }
+                catch
+                {
+                    return RedirectToAction("Index", "Manage", new { email = email });
+                }
+            }
         }
     }
 }
